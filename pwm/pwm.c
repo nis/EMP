9bb7416c023f44b9435248697987c93db1ef1ad2
Nis Sarup <nis@sarup.dk>
2011-03-20 22:38:09 +0100

    Added init and duty cycle method for PWM. Sometimes the fan refuses to start. I hev no idea why.

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -25,11 +25,70 @@
 
 /*****************************    Defines    *******************************/
 
+#define PWM_PERIOD 		12000
+#define PWM_LOAD  		(PWM_PERIOD-1)
+
 /*****************************   Constants   *******************************/
 
 /*****************************   Variables   *******************************/
 
 /*****************************   Functions   *******************************/
+
+void init_pwm(void)
+/*****************************************************************************
+*   Function : See h-file for specification.
+*****************************************************************************/
+{
+	SYSCTL_RCGC0_R |= SYSCTL_RCC_USEPWMDIV; // Enable clock
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOB;
+	
+	INT8U dummy = SYSCTL_RCGC2_R; // Dummy! Yay!
+	
+	
+	GPIO_PORTB_AFSEL_R |= 0x03;	// Alt conf.
+	GPIO_PORTB_DEN_R |= 0x03;
+	
+	SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV; // RCC uses PWM divide and divide by two
+	SYSCTL_RCC_R &= ~SYSCTL_RCC_PWMDIV_M;
+	SYSCTL_RCC_R |= SYSCTL_RCC_PWMDIV_2;
+	
+	PWM_1_CTL_R = 0;	// PWM countdown
+	PWM_1_GENA_R |= 0x8C;
+	PWM_1_GENB_R |= 0x80C;
+	
+	PWM_1_LOAD_R |= PWM_LOAD; // The period
+	
+	PWM_1_CMPA_R = 0; // Pulse width, PWM2 pin
+	
+	PWM_INVERT_R |= PWM_INVERT_PWM2INV;
+	
+	PWM_1_CTL_R |= 0x1; // Start PWM generator 1 timers
+	
+	PWM_ENABLE_R |= 0xC; // Enable outputs
+}
+
+void pwm_set_duty_cycle(INT16U dc)
+/*****************************************************************************
+*   Function : See h-file for specification.
+*****************************************************************************/
+{
+	dc = (12000/100)*dc;
+	
+	if(dc > PWM_LOAD)
+	{
+		dc = PWM_LOAD - 1;
+	}
+	
+	PWM_1_CMPA_R = dc;
+}
+
+void pwm_task(void)
+/*****************************************************************************
+*   Function : See h-file for specification.
+*****************************************************************************/
+{
+	
+}
 
 /****************************** End Of Module *******************************/
 
