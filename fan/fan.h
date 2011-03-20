923c0039d651a207d2dba5afe688c4a007174fc7
Nis Sarup <nis@sarup.dk>
2011-03-20 18:56:56 +0100

    Added a getter for the fan ref. speed.

--- fan/fan.h
+++ fan/fan.h
@@ -8,7 +8,12 @@
 #ifndef FAN_H_
 #define FAN_H_
 
-
+INT8U fan_get_ref_speed(void);
+/*****************************************************************************
+*   Input    : 	-
+*   Output   : 	-
+*   Function : 	Returns the ref speed of the fan.
+******************************************************************************/
 
 
 #endif /* FAN_H_ */
